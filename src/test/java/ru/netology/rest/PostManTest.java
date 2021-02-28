93a7bfe8bdb2758d222c89ee01ce7a6a2b44de91
balrom1981 <balrom@bk.ru>
2021-02-28 21:20:17 +0300

    test added

--- src/test/java/ru/netology/rest/PostManTest.java
+++ src/test/java/ru/netology/rest/PostManTest.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 class PostManTest {
     @Test
@@ -11,13 +12,14 @@ class PostManTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("bar1") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("bar1"))
         ;
     }
+}
